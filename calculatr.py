95ed72de78e074289d15dcfc6579bc0c2a4b29ab
P-Cod3 <91882725+P-Cod3@users.noreply.github.com>
2021-11-08 00:41:20 +0300

    first uploads

---
+++ calculatr.py
@@ -0,0 +1,57 @@
+﻿# program for a simple calculator
+import math
+# a function that adds two numbers
+def add(x, y):
+    return x + y
+
+# a function that subtracts two numbers
+def subtract(x, y):
+    return x - y
+
+# a function that multiplies two numbers
+def multiply(x, y):
+    return x * y
+
+# a function that divides two numbers
+def divide(x, y):
+    return x / y
+# a function that finds the square root of X (X>0)
+def square_root(x):
+   return  math.sqrt(x)
+
+print("Choose operation.")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.SquareRoot")
+while True:
+    # takes input from the user
+    choice = input("Enter choice(1/2/3/4/5): ")
+
+    # check if choice is one of the four options
+    if choice == '5':
+        x =float(input("Enter the number to find square root:"))
+        print("Square Root of",x,"is",square_root(x))
+
+    elif choice in ('1', '2', '3', '4'):
+        num1 = float(input("Enter first number: "))
+        num2 = float(input("Enter second number: "))
+
+        if choice == '1':
+            print(num1, "+", num2, "=", add(num1, num2))
+
+        elif choice == '2':
+            print(num1, "-", num2, "=", subtract(num1, num2))
+
+        elif choice == '3':
+            print(num1, "*", num2, "=", multiply(num1, num2))
+
+        elif choice == '4':
+            print(num1, "/", num2, "=", divide(num1, num2))
+        
+        # check if user wants another calculation
+        # break the while loop if answer is no
+    next_calculation = input("Let's do next calculation? (yes/no): ")
+    if next_calculation == "no":
+        break
---
+++ program_for_displaying_legible_voter.py
@@ -0,0 +1,15 @@
+#program to determine whether a number is positive or negative 
+#display appropriate message 
+num = int(input("Input  number to test:"))
+if num >= 0:
+    print ("positive or zero")
+
+else:
+     print("negative number")
+#program to determine legibility of voting
+#And display appropriate message 
+age =int(input("Input Age of prospective voter:"))
+if age >17:
+  print("legible to vote")
+else:
+    print("illegible to vote")
